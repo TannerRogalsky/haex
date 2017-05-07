6ef6f71e44eda90cf89878b23aeba4b3fac49534
Tanner Rogalsky <tanner@tannerrogalsky.com>
2017-05-07 17:51:16 -0400

    conf

--- conf.lua
+++ conf.lua
@@ -1,11 +1,11 @@
 function love.conf(t)
-  t.identity = "love2d_template"      -- The name of the save directory (string)
+  t.identity = "haex"      -- The name of the save directory (string)
   t.version = "0.10.1"                -- The LÖVE version this game was made for (string)
   t.console = false                   -- Attach a console (boolean, Windows only)
   t.accelerometerjoystick = true      -- Enable the accelerometer on iOS and Android by exposing it as a Joystick (boolean)
   t.gammacorrect = false              -- Enable gamma-correct rendering, when supported by the system (boolean)
 
-  t.window.title = "Template"         -- The window title (string)
+  t.window.title = "HAEX"         -- The window title (string)
   t.window.icon = nil                 -- Filepath to an image to use as the window's icon (string)
   t.window.width = 1280               -- The window width (number)
   t.window.height = 700               -- The window height (number)
@@ -38,4 +38,18 @@ function love.conf(t)
   t.modules.video = true              -- Enable the video module (boolean)
   t.modules.window = true             -- Enable the window module (boolean)
   t.modules.thread = true             -- Enable the thread module (boolean)
+
+  t.releases = {
+    title = t.window.title,
+    package = 'haex',
+    loveVersion = t.version,
+    version = nil,
+    author = 'Tanner Rogalsky',
+    email = 'tanner@tannerrogalsky.com',
+    description = nil,
+    homepage = nil,
+    identifier = t.identity,
+    releaseDirectory = '../love2d_builds',
+    excludeFileList = { 'docs/', '%.rockspec', 'source_images/' }
+  }
 end
